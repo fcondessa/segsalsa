6729d955b9cc37c092d6508e4048dfc47d4c7e9d
filipe <fcondessa@gmail.com>
2016-05-03 11:33:34 -0400

    renamed example_hyperspectral to example_hyperspectral_indian

--- example_hyperspectral_indian.m
+++ example_hyperspectral_indian.m
@@ -15,7 +15,10 @@ segmentations;
 % image of gradients
 weight_image;
 
-
+szimg = size(false_color_image);
+[X1,Y1] = gradient(double(mask));
+delim = X1|Y1;
+white = cat(3,ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)));
 %% local clusters
 % parameter definition
 VIS_TAG = 0;
@@ -28,6 +31,11 @@ Z0 = segsalsa(probabilities,mu,iterations,...
 
 figure(123)
 imshow(d0,[]);colormap('jet')
+hold on;
+white = cat(3,ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)));
+h= imshow(white);
+hold off;
+set(h,'AlphaData',delim)
 title(['GTV only. Acc = ' num2str(mean(d0(mask(:)) == ground_truth(mask(:))))]);
 %% VTV only
 Z0 = segsalsa(probabilities,mu,iterations,...
@@ -36,15 +44,25 @@ Z0 = segsalsa(probabilities,mu,iterations,...
 
 figure(124)
 imshow(d0,[]);colormap('jet')
+hold on;
+white = cat(3,ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)));
+h= imshow(white);
+hold off;
+set(h,'AlphaData',delim)
 title(['VTV only. Acc = ' num2str(mean(d0(mask(:)) == ground_truth(mask(:))))]);
 %% GTV and VTV
 Z0 = segsalsa(probabilities,mu,iterations,...
     'VTV',1,'tau_vtv',10/2,'weight_image_vtv',weight_image,...
-    'GTV',1,'tau_gtv',[1 2 5 10]/2,'clusters',segmentations,'VIS',1);
+    'GTV',1,'tau_gtv',[1 2 5 10]/2,'clusters',segmentations,'VIS',VIS_TAG);
 [c0,d0] = max(Z0,[],3);
 
 figure(125)
 imshow(d0,[]);colormap('jet')
+hold on;
+white = cat(3,ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)));
+h= imshow(white);
+hold off;
+set(h,'AlphaData',delim)
 title(['GTV and VTV. Acc = ' num2str(mean(d0(mask(:)) == ground_truth(mask(:))))]);
 
 %% STR only
@@ -54,6 +72,11 @@ Z0 = segsalsa(probabilities,mu,iterations,...
 
 figure(126)
 imshow(d0,[]);colormap('jet')
+hold on;
+white = cat(3,ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)));
+h= imshow(white);
+hold off;
+set(h,'AlphaData',delim)
 title(['STR only. Acc = ' num2str(mean(d0(mask(:)) == ground_truth(mask(:))))]);
 
 
@@ -65,6 +88,11 @@ Z0 = segsalsa(probabilities,mu,iterations,...
 
 figure(127)
 imshow(d0,[]);colormap('jet')
+hold on;
+white = cat(3,ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)));
+h= imshow(white);
+hold off;
+set(h,'AlphaData',delim)
 title(['STR and VTV Acc = ' num2str(mean(d0(mask(:)) == ground_truth(mask(:))))]);
 
 %% STR and GTV
@@ -73,6 +101,14 @@ Z0 = segsalsa(probabilities,mu,iterations,...
     'GTV',1,'tau_gtv',[1 2 5 10]/2,'clusters',segmentations,'VIS',VIS_TAG );
 [c0,d0] = max(Z0,[],3);
 
+
 figure(128)
 imshow(d0,[]);colormap('jet')
-title(['STR and GTV Acc = ' num2str(mean(d0(:) == ground_truth(:)))]);
+hold on;
+white = cat(3,ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)),ones(szimg(1),szimg(2)));
+h= imshow(white);
+hold off;
+set(h,'AlphaData',delim)
+
+
+title(['STR and GTV Acc = ' num2str(mean(d0(mask(:)) == ground_truth(mask(:))))]);
